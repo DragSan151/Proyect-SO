53b99ca4beb60880abc68b656311720f82db522c
ALEJOGod <alejo04cl@gmail.com>
2021-11-09 20:04:24 -0500

    Agrege el Gerente.sh

--- Gerente.sh
+++ Gerente.sh
@@ -22,12 +22,14 @@ echo "0) Salir"
 read opc
 case $opc in
 
-              1)echo "Dar alta a un Administrativo"
-        read -p "Ingrese el nombre: " nombre
-        read -p "Ingrese el password: " password
-        read -p "Ingrese la cedula:  " cedula
-        Adminis=$( grep "$nombre" Usuario.txt |  cut -f 3 -d":" )
-	NombreIgual=$( grep "$nombre" Usuario.txt | cut -f 1 -d":"  )
+1)
+echo "Dar alta a un Administrativo"
+if [ -f Usuario.txt ]
+then
+read -p "Ingrese el nombre: " nombre
+read -p "Ingrese el password: " password
+read -p "Ingrese la cedula:  " cedula
+NombreIgual=$( grep "$nombre" Usuario.txt | cut -f 1 -d":"  )
 
 
 	if [ "$NombreIgual" == "$nombre" ]
@@ -39,16 +41,21 @@ case $opc in
         echo $nombre:$password:Administrativo:$cedula >>Usuario.txt
         cat Usuario.txt
 	fi
-
+else
+echo "No existe el Fichero"
+fecha=$( date +'%d/%m/%Y %H:%M' )
+echo "Usuario.txt no existe $fecha" >> log.txt
+fi
 ;;
 
-              2)echo "Dar debaja a Administrativo"
-        cat Usuario.txt
-        read -p "Ingrese el Nombre " nombre
-        Adminis=$( grep "$nombre" Usuario.txt | cut -f 3 -d":" )
-
-         if [ "$Adminis" == "Adminsitrativo" ]
-         then
+2)
+echo "Dar debaja a Administrativo"
+cat Usuario.txt
+read -p "Ingrese el Nombre " nombre
+Adminis=$( grep "$nombre" Usuario.txt | cut -f 3 -d":")
+echo "$Adminis"
+if [ "$Adminis" == "Administrativo" ]
+then
           NombreIgual=$( grep "$nombre" Usuario.txt | cut -f 1 -d":"  )
 
 	if [ "$NombreIgual" == "$nombre" ]
@@ -67,19 +74,19 @@ case $opc in
         echo "El Administrativo no se encontro $nombre $fecha" >> log.txt
         fi
 
-         else
-         echo "Error al ingresar el nombre del Administrativo"
-         fecha=$( date +'%d/%m/%Y %H:%M' )
-        echo "El Administrativo no se encontro $nombre $fecha" >> log.txt
-         fi
+else
+echo "Error al ingresar el nombre del Administrativo"
+fecha=$( date +'%d/%m/%Y %H:%M' )
+echo "El Administrativo no se encontro $nombre $fecha" >> log.txt
+fi
 ;;
 
-              3)opciones=6
-	clear
-	while [ "$opciones" != "0" ]
+3)opciones=6
+clear
+while [ "$opciones" != "0" ]
 
-	do
-    	echo   "Modificar datos"
+do
+echo   "Modificar datos"
      	 echo   "Que dato quieres modificar  "
      	 echo   "1)Contraseña"
      	 echo   "2)Cedula"
@@ -131,54 +138,163 @@ case $opc in
 	done
 	;;
               
-              4)echo "Listando Administrativos"
-	cat Usuario.txt
-              ;;
-              5)if [ -f Usuario.txt ]
-	then
-		opcion=5
+4)echo "Listando Administrativos"
+cat Usuario.txt
+;;
+5)
+echo "Buscar un Administrativo"
+read -p "Ingrese el nombre del Administrativo" nombre
+Verificar=$(grep "$nombre" Usuarios.txt | cut -f 3 -d":")
+if [ "$Verificar" == "Administrativo" ]
+then
 
-		while [ "$opcion" != "0"  ]
+if [ -f Usuario.txt ]
+then
 
-		do
+opcion=3
 
-       		echo "Buscar datos de usuario"
-       		echo "1)Buscar por nombre"
-       		 echo "2)Buscar por password"
-       		 echo "3)Buscar por la cedula"
-      	  	 echo "0)Volver al menu"
-		read opcion
-		case $opcion in
-        	1) 
-		cut -f 1 -d : Usuario.txt
-       		 ;;
-       		 2) cut -f 2 -d : Usuario.txt
-       		 ;;
-       		 3) cut -f 3 -d : Usuario.txt
-       		 ;;
-       		 0)
-		echo "Salio"
-		;;
-		*)
-		echo "Error en el ingreso"
-		;;
-		esac
-		done
-	else
-		echo "El fichero no existe"
-		fecha=$( date +'%d/%m/%Y %H:%M' )
-	        echo "No existe el fichero Usuario.txt $fecha" >> log.txt
+while [ "$opcion" != "0"  ]
+do
 
+echo "Buscar datos de usuario"
+echo "1)Buscar la Contraseña"
+echo "2)Buscar la Cedula"
+echo "0)Volver al menu"
+read opcion
+case $opcion in
 
-	fi
-              ;;
-              6)echo "Listando Clientes"
-	cat Clientes.txt
-              ;;
-              7)
-              ;;
-              0)exit
-              ;;
+1)
+read -p "Ingrese el nombre para buscar el usuario " nombre
+
+NombreIgual=$( grep "$nombre" Usuario.txt | cut -f 1 -d":"  )
+
+linea=$( grep "$nombre" Usuario.txt | cut -f 2 -d":" )
+
+if [ "$nombre" == "$NombreIgual" ]
+then
+echo "$linea"
+else
+echo "No existe la contraseña"
+fi
+;;
+
+2)
+read -p "Ingrese el nombre para buscar el usuario " nombre
+
+NombreIgual=$( grep "$nombre" Usuario.txt | cut -f 1 -d":"  )
+
+linea=$( grep "$nombre" Usuario.txt | cut -f 4 -d":" )
+
+if [ $nombre == $NombreIgual ]
+then
+echo "$linea"
+else
+echo "No existe la cedula"
+fi
+;;
+0)
+echo "Has Salido"
+;;
+*)
+echo "Error en el ingreso"
+;;
+
+esac
+done
+else
+fecha=$( date +'%d/%m/%Y %H:%M' )
+echo "Usuario.txt no existe $fecha" >> log.txt
+echo "Usuario.txt no existe"
+fi
+else
+fecha=$( date +'%d/%m/%Y %H:%M' )
+echo "El Administrativo no se encontro $nombre $fecha" >> log.txt
+echo "El Administrativo no se ha encontrado"
+fi
+
+;;
+6)
+echo "Listando Clientes"
+cat Clientes.txt
+;;
+7)
+if [ -f Clientes.txt ]
+then
+
+opcion=4
+
+while [ "$opcion" != "0"  ]
+do
+
+echo "Buscar datos de usuario"
+echo "1)Buscar el nombre"
+echo "2)Buscar el apellido"
+echo "3)Buscar la fecha"
+echo "0)Volver al menu"
+read opcion
+case $opcion in
+
+1)
+read -p "Ingrese la cedula para buscar el cliente " cedula
+
+CedulaIgual=$( grep "$cedula" Clientes.txt | cut -f 3 -d":"  )
+
+linea=$( grep "$cedula" Clientes.txt  | cut -f 1 -d":" )
+
+if [ "$CedulaIgual" == "$cedula" ]
+then
+echo "$linea"
+else
+echo "No existe la cedula"
+fi
+;;
+
+2)
+read -p "Ingrese la cedula para buscar el cliente " cedula
+
+CedulaIgual=$( grep "$cedula" Clientes.txt | cut -f 3 -d":"  )
+
+linea=$( grep "$cedula" Clientes.txt  | cut -f 2 -d":" )
+
+if [ $CedulaIgual == $cedula ]
+then
+echo "$linea"
+else
+echo "No existe la cedula"
+fi
+;;
+3)
+read -p "Ingrese la cedula para buscar el cliente " cedula
+
+CedulaIgual=$( grep "$cedula" Clientes.txt | cut -f 3 -d":"  )
+
+linea=$( grep "$cedula" Clientes.txt  | cut -f 4 -d":" )
+if [ $CedulaIgual == $cedula ]
+then
+echo "$linea"
+else
+echo "No existe la cedula"
+fi
+
+;;
+0)
+echo "Has Salido"
+;;
+*)
+echo "Error en el ingreso"
+;;
+
+esac
+done
+
+else
+echo "El fichero no existe"
+fecha=$( date +'%d/%m/%Y %H:%M' )
+echo "No existe el fichero Usuario.txt $fecha" >> log.txt
+fi
+;;
+0)exit
+;;
 
 esac
 done
